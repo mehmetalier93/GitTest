0d4a09daf39a9168a0aa4bdc4e9fa74e6c7f51cb
Mehmet Ali Er <mehmetalier93@gmail.com>
2020-11-09 14:18:39 +0300

    Initial Commit

---
+++ GitTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTestProject
+//
+//  Created by Mehmet Ali Er on 9.11.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
